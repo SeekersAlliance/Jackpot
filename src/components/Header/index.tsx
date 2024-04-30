777a098d06158cf72a3a5683bab04be644182599
FME849 <1414439@gmail.com>
2024-04-30 20:48:26 +0700

    feat: impl UI layout for buy cards page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,99 @@
+import type { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const tabs = [
+	{
+		link: '/',
+		img: 'img/buttons/buy_cards.png',
+		imgActive: 'img/buttons/buy_cards_onpage.png',
+	},
+	{
+		link: '/inventory',
+		img: 'img/buttons/inventory.png',
+		imgActive: 'img/buttons/inventory_onpage.png',
+	},
+	{
+		link: '/dashboard',
+		img: 'img/buttons/dashboard.png',
+		imgActive: 'img/buttons/dashboard_onpage.png',
+	},
+];
+
+export const Header: FC = () => {
+	const navigate = useNavigate();
+
+	return (
+		<Container>
+			<div style={{ flex: 1 }}>
+				<NavigateGroup>
+					{tabs.map((tab) => {
+						return (
+							<img
+								key={tab.link}
+								src={tab.img}
+								onClick={() => navigate(tab.link)}
+							/>
+						);
+					})}
+				</NavigateGroup>
+			</div>
+			<div style={{ flex: 1 }}>
+				<Jackpot src="img/pg1-2/jackpot.png" />
+			</div>
+			<div style={{ flex: 1, justifyContent: 'flex-end' }}>
+				<ConnectWallet
+					onClick={() => {
+						console.log('coming soon');
+					}}
+				>
+					Connect Wallet
+				</ConnectWallet>
+			</div>
+		</Container>
+	);
+};
+
+export default Header;
+
+const Container = styled.div`
+	flex-direction: row;
+	padding: 0 15px;
+	width: 100%;
+	justify-content: space-between;
+	align-self: start;
+`;
+
+const NavigateGroup = styled.div`
+	gap: 15px;
+	align-self: center;
+
+	img {
+		max-width: 100px;
+		cursor: pointer;
+	}
+`;
+
+const Jackpot = styled.img`
+	max-width: 500px;
+	aspect-ratio: 827 / 217;
+	height: auto;
+	object-fit: contain;
+`;
+
+const ConnectWallet = styled.div`
+	width: 250px;
+	aspect-ratio: 328 / 146;
+	align-items: center;
+	justify-content: center;
+	background-image: url(img/buttons/connect.png);
+	background-size: contain;
+	background-repeat: no-repeat;
+	background-position: center;
+	font-size: 25px;
+	font-weight: 700;
+	font-family: 'TitilliumWeb';
+	align-self: center;
+	user-select: none;
+	cursor: pointer;
+`;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,25 +1,61 @@
 import type { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Fragment } from 'react';
+import FaucetBtn from 'components/FaucetBtn';
+import Header from 'components/Header';
 import styled from 'styled-components';
 
 export const HomeScreen: FC = () => {
-	const navigate = useNavigate();
-
 	return (
-		<Container className="container-block bgsize">
-			<PageTitle onClick={() => navigate('/')}>coming soon..</PageTitle>
-		</Container>
+		<Fragment>
+			<Container>
+				<Header />
+				<Content>
+					<BtnGroup>
+						<FaucetBtn>TBNB Faucet</FaucetBtn>
+						<FaucetBtn>Get 1000 TestUSD</FaucetBtn>
+					</BtnGroup>
+					<MainContent>
+						<img src="img/pg1-2/buy_cards_banner.png" />
+					</MainContent>
+				</Content>
+			</Container>
+			<SeekersAllianceLogo src="img/pg1-2/seekers_alliance_logo.png" />
+		</Fragment>
 	);
 };
 
 export default HomeScreen;
 
 const Container = styled.div`
-	flex: 1;
-	justify-content: center;
+	flex-direction: column;
 	align-items: center;
+	height: 100vh;
 `;
 
-const PageTitle = styled.span`
-	color: #ffffff;
+const Content = styled.div`
+	height: 100%;
+	flex-direction: column;
+	justify-content: flex-end;
+	gap: 15px;
+	padding-bottom: 80px;
+`;
+
+const BtnGroup = styled.div`
+	gap: 15px;
+`;
+
+const MainContent = styled.div`
+	position: relative;
+
+	img {
+		max-width: 1200px;
+		object-fit: contain;
+		aspect-ratio: 20 / 9;
+	}
+`;
+
+const SeekersAllianceLogo = styled.img`
+	position: absolute;
+	bottom: 15px;
+	right: 15px;
 `;
